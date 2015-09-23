f42491bce0e287adf089a9bd1298f441a80b294a
Mariusz Wyrozumski <mariusz.wyrozumski@gmail.com>
2015-09-23 23:16:34 +0200

    Move http_message and rule_message to a header file

--- rules_parser.c
+++ rules_parser.c
@@ -2,17 +2,8 @@
 #include <stdlib.h>
 
 #include "http.h"
+#include "rules_parser.h"
 
-
-typedef struct http_message_t rule_request_t;
-
-typedef struct http_message_t rule_response_t;
-
-struct rule_message_t {
-  rule_request_t *request;
-  rule_response_t *response;
-  struct rule_message_t *next;
-};
 
 FILE* read_rules_file(const char *path) {
   FILE *file = fopen(path, "r");
--- rules_parser.h
+++ rules_parser.h
@@ -5,4 +5,13 @@
 
 FILE* read_rules_file(const char* path);
 
+typedef struct http_message_t rule_request_t;
+typedef struct http_message_t rule_response_t;
+
+struct rule_message_t {
+  rule_request_t *request;
+  rule_response_t *response;
+  struct rule_message_t *next;
+};
+
 #endif
