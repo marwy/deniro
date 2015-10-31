265f95f9bde1a44e2b46973af7a09ce91156ce15
Mariusz Wyrozumski <mariusz.wyrozumski@gmail.com>
2015-10-31 22:05:01 +0100

    Make rule's request and response to be their own structs

--- rules_parser.h
+++ rules_parser.h
@@ -2,16 +2,24 @@
 #define RULES_PARSER_H
 
 #include <stdio.h>
+#include "http.h"
 
 char* read_rules_file(const char* path);
 struct rule_message_t *parse_rules(char *rules_string);
 
-typedef struct http_message_t rule_request_t;
-typedef struct http_message_t rule_response_t;
+struct rule_request_t {
+  struct http_request_t *super;
+  const char *identifier;
+};
+
+struct rule_response_t {
+  struct http_response_t *super;
+  const char *identifier;
+};
 
 struct rule_message_t {
-  rule_request_t *request;
-  rule_response_t *response;
+  struct rule_request_t *request;
+  struct rule_response_t *response;
   struct rule_message_t *next;
 };
 
