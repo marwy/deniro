6fdbcfb9f693bf98d27a8b1b7ada1604c238b614
Mariusz Wyrozumski <mariusz.wyrozumski@gmail.com>
2015-10-31 21:05:43 +0100

    Seperate out the HTTP message to smaller, better conforming to the spec structs

--- http.h
+++ http.h
@@ -1,6 +1,8 @@
 #ifndef HTTP_H
 #define HTTP_H
 
+#include <stdint.h>
+
 enum HTTP_METHOD {
   HEAD,
   GET,
@@ -10,17 +12,33 @@ enum HTTP_METHOD {
   DELETE
 };
 
-struct http_message_header {
+struct http_header_t {
   char *name;
   char *value;
-  struct http_message_header *next_header;
+  struct http_header_t *next_header;
 };
 
-struct http_message_t {
-  const char *name;
-  const char *url;
+struct http_request_line_t {
   enum HTTP_METHOD method;
+  const char *url;
+  const char *http_version;
+};
+
+struct http_request_t {
+  struct http_request_line_t *request_line;
+  struct http_header_t *headers;
   const char *body;
-  struct http_message_header *header_start;
+};
+
+struct http_status_line_t {
+  const char *http_version;
+  uint16_t      status_code;
+  const char *reason_phrase;
+};
+
+struct http_response_t {
+  struct http_status_line_t *status_line;
+  struct http_header_t *headers;
+  const char *body;
 };
 #endif
