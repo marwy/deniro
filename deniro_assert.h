89df4b8487425bdd5311a9b0833001a23d25948a
Mariusz Wyrozumski <mariusz.wyrozumski@gmail.com>
2015-11-19 11:03:54 +0100

    Add basic testing macros

---
+++ deniro_assert.h
@@ -0,0 +1,16 @@
+#ifndef DENIRO_ASSERT_H
+#define DENIRO_ASSERT_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PRINT_FAIL printf("************ FAIL IN FILE %s LINE %d\n", \
+                          __FILE__, __LINE__);
+
+#define den_assert(test) if (!(test)) {  PRINT_FAIL }
+
+#define den_assert_str_eq(str1, str2) if (!(strcmp(str1, str2) == 0)) { \
+    printf("*********** STRINGS DON'T MATCH expected: \"%s\" \t got: \"%s\" \t", \
+           str2, str1); PRINT_FAIL }
+
+#endif
