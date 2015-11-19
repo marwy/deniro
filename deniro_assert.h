c4cb19c97191a07a291202e9960f64e6eed44929
Mariusz Wyrozumski <mariusz.wyrozumski@gmail.com>
2015-11-19 19:53:52 +0100

    Add NULL checking in assert string equality macro

--- deniro_assert.h
+++ deniro_assert.h
@@ -9,7 +9,9 @@
 
 #define den_assert(test) if (!(test)) {  PRINT_FAIL }
 
-#define den_assert_str_eq(str1, str2) if (!(strcmp(str1, str2) == 0)) { \
+#define den_assert_str_eq(str1, str2) \
+  if (!(str1)) { printf("Expected value is NULL\n"); PRINT_FAIL } \
+  if (!(strcmp(str1, str2) == 0)) { \
     printf("*********** STRINGS DON'T MATCH expected: \"%s\" \t got: \"%s\" \t", \
            str2, str1); PRINT_FAIL }
 
